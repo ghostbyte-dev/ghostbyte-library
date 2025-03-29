3cb9afb5f43799bc7f233f5dee0846c3db71634b
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-03-29 12:37:58 +0100

    add prerendering

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,24 +1,33 @@
 import Link from "next/link";
 import Collection from "../../components/Collection";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ category: string }>;
-}) {
-  const { category } = await params;
+export async function generateStaticParams() {
+  const categories = [
+    "ai-art",
+    "fonts",
+    "extensions",
+    "best-practices",
+    "illustrations",
+    "stock-photos",
+    "colors",
+    "inspiration",
+    "ui-components",
+    "tools",
+    "design-systems",
+    "games",
+    "systems",
+    "icons",
+  ];
 
-  console.log(category);
+  return categories.map((category) => ({ category }));
+}
+
+export default async function Page({ params }: { params: { category: string } }) {
+  const { category } = await params; // Ensure params is awaited
 
   return (
     <div className="w-full md:ml-64">
-      {!category ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <Collection collection={category} />
-        </>
-      )}
+      <Collection collection={category} />
 
       <div className="mt-10 pb-10 text-center text-white">
         Developed with ❤️ by{" "}
