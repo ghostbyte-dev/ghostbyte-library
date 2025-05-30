e7f0be15e23a4be9fba08079de7c22a06c8acfea
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-05-30 22:31:03 +0200

    Fix penpot link

--- data/design/tools.ts
+++ data/design/tools.ts
@@ -1,153 +1,153 @@
 import { ICollection } from "../../src/types/types";
 
 const designToolsData: ICollection = {
-  name: "Design Tools",
-  introduction: "A collection of powerful design-focused tools to enhance your creative workflow. From SEO checkers and pattern generators to mockup creators and image optimizers, these resources help you refine and perfect your designs with ease.",
-  items: [
-    {
-      name: "Toptal Checklist",
-      description: "Web Developer Checklist",
-      url: "https://www.toptal.com/developers/webdevchecklist",
-      logo: "design/tools/toptal-checklist.png",
-      screenshot: "design/tools/screen_toptalchecklist.png",
-      tags: [
-        {
-          name: "free"
-        }
-      ]
-    },
-    {
-      name: "Patternico",
-      description: "Pattern generator",
-      url: "https://patternico.com/",
-      logo: "design/tools/patternico.webp",
-      screenshot: "design/tools/screen_patternico.png",
-      tags: [
-        {
-          name: "free"
-        }
-      ]
-    },
-    {
-      name: "The good line-height",
-      description: "Calculate perfect line height",
-      url: "https://www.thegoodlineheight.com",
-      logo: "design/tools/thegoodlineheight.svg",
-      screenshot: "design/tools/screen_goodlineheight.png",
-      tags: [
-        {
-          name: "free"
-        }
-      ]
-    },
-    {
-      name: "Pattern Monster",
-      description: "Customizable SVG patterns",
-      url: "https://pattern.monster/",
-      logo: "design/tools/patternmonster.png",
-      screenshot: "design/tools/screen_patternmonster.png",
-      tags: [
-        {
-          name: "os"
-        },
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "CleanUp",
-      description: "Editing Tool to remove unwanted elements from image",
-      url: "https://cleanup.pictures",
-      logo: "design/tools/cleanup.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "Figma",
-      description: "Design tool",
-      url: "https://www.figma.com/",
-      logo: "design/tools/figma.png",
-      screenshot: "design/tools/screen_figma.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "Compressor",
-      description: "Fast & effective image compression",
-      url: "https://compressor.io",
-      logo: "design/tools/compressor.png",
-      screenshot: "design/tools/screen_compressor.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "Zeplin",
-      description: "Design tool",
-      url: "https://zeplin.io/",
-      logo: "design/tools/zeplin.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "Remix",
-      description: "Transform any link, tweet, or product into beautiful Instagram content",
-      url: "https://buffer.com/remix",
-      logo: "design/tools/remix.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "Canva",
-      description: "Online design tool",
-      url: "https://www.canva.com/",
-      logo: "design/tools/canva.jpg",
-      screenshot: "design/tools/screen_canva.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    },
-    {
-      name: "Penpot",
-      description: "Penpot is an Open Source design and prototyping platform that enables collaborative work with a suite of tools designed for both designers and developers.",
-      url: "https://https://penpot.app/",
-      logo: "design/tools/penpot.png",
-      screenshot: "design/tools/screen_canva.png",
-      tags: [
-        { name: "freemium" },
-        { name: "os" }
-      ]
-    },
-    {
-      name: "haikei",
-      description: "Generate unique SVG design assets",
-      url: "https://haikei.app/",
-      logo: "design/tools/haikei.png",
-      screenshot: "design/tools/screen_haikei.png",
-      tags: [
-        {
-          name: "freemium"
-        }
-      ]
-    }
-  ]
+	name: "Design Tools",
+	introduction:
+		"A collection of powerful design-focused tools to enhance your creative workflow. From SEO checkers and pattern generators to mockup creators and image optimizers, these resources help you refine and perfect your designs with ease.",
+	items: [
+		{
+			name: "Toptal Checklist",
+			description: "Web Developer Checklist",
+			url: "https://www.toptal.com/developers/webdevchecklist",
+			logo: "design/tools/toptal-checklist.png",
+			screenshot: "design/tools/screen_toptalchecklist.png",
+			tags: [
+				{
+					name: "free",
+				},
+			],
+		},
+		{
+			name: "Patternico",
+			description: "Pattern generator",
+			url: "https://patternico.com/",
+			logo: "design/tools/patternico.webp",
+			screenshot: "design/tools/screen_patternico.png",
+			tags: [
+				{
+					name: "free",
+				},
+			],
+		},
+		{
+			name: "The good line-height",
+			description: "Calculate perfect line height",
+			url: "https://www.thegoodlineheight.com",
+			logo: "design/tools/thegoodlineheight.svg",
+			screenshot: "design/tools/screen_goodlineheight.png",
+			tags: [
+				{
+					name: "free",
+				},
+			],
+		},
+		{
+			name: "Pattern Monster",
+			description: "Customizable SVG patterns",
+			url: "https://pattern.monster/",
+			logo: "design/tools/patternmonster.png",
+			screenshot: "design/tools/screen_patternmonster.png",
+			tags: [
+				{
+					name: "os",
+				},
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "CleanUp",
+			description: "Editing Tool to remove unwanted elements from image",
+			url: "https://cleanup.pictures",
+			logo: "design/tools/cleanup.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "Figma",
+			description: "Design tool",
+			url: "https://www.figma.com/",
+			logo: "design/tools/figma.png",
+			screenshot: "design/tools/screen_figma.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "Compressor",
+			description: "Fast & effective image compression",
+			url: "https://compressor.io",
+			logo: "design/tools/compressor.png",
+			screenshot: "design/tools/screen_compressor.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "Zeplin",
+			description: "Design tool",
+			url: "https://zeplin.io/",
+			logo: "design/tools/zeplin.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "Remix",
+			description:
+				"Transform any link, tweet, or product into beautiful Instagram content",
+			url: "https://buffer.com/remix",
+			logo: "design/tools/remix.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "Canva",
+			description: "Online design tool",
+			url: "https://www.canva.com/",
+			logo: "design/tools/canva.jpg",
+			screenshot: "design/tools/screen_canva.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+		{
+			name: "Penpot",
+			description:
+				"Penpot is an Open Source design and prototyping platform that enables collaborative work with a suite of tools designed for both designers and developers.",
+			url: "https://penpot.app/",
+			logo: "design/tools/penpot.png",
+			screenshot: "design/tools/screen_canva.png",
+			tags: [{ name: "freemium" }, { name: "os" }],
+		},
+		{
+			name: "haikei",
+			description: "Generate unique SVG design assets",
+			url: "https://haikei.app/",
+			logo: "design/tools/haikei.png",
+			screenshot: "design/tools/screen_haikei.png",
+			tags: [
+				{
+					name: "freemium",
+				},
+			],
+		},
+	],
 };
 
 export default designToolsData;
